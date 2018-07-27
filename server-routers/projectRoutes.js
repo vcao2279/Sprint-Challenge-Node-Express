6b10bd2072013d834ffafa1e42fc09e35b0bdf12
Vu Cao <cdhvu@yahoo.com>
2018-07-27 09:12:48 -0700

    Create endpoint for GET project with id and GET project's actions

--- server-routers/projectRoutes.js
+++ server-routers/projectRoutes.js
@@ -8,11 +8,11 @@ function sendError(code, message, error) {
     return {
         code: code,
         message: message,
-        error: error.message
+        error: error
     }
 }
 
-//endpoint for projects GET
+//endpoint for GET
 router.get('/', async (req, res, next) => {
     try {
         const response = await db.get();
@@ -21,5 +21,39 @@ router.get('/', async (req, res, next) => {
         next(sendError(500, 'Projects information could not be retrieve.', error))
     }
 })
+
+//endpoint for GET with id
+router.get('/:id', async(req, res, next) => {
+    const id = req.params.id;
+    
+    try {
+        const response = await db.get(id);
+        res.status(200).json(response);
+    } catch (error) {
+        next(sendError(500, 'Project information could not be retrieve.', error))
+    }
+})
+
+//endpoint for GET project actions
+router.get('/:id/actions', async(req, res, next) => {
+    const id = req.params.id;
+    
+    try {
+        const response = await db.getProjectActions(id);
+        if (response.length===0) {
+            return next(sendError(404, "Project's actions information could not be retrieve.", "There is no project for this specified ID."))
+        }
+
+        res.status(200).json(response);
+    } catch (error) {
+        next(sendError(500, "Project's actions information could not be retrieve.", error.message))
+    }
+})
+
+//endpoint for POST 
+
+//endpoint for DELETE
+
+//endpoint for UPDATE
 
 module.exports = router;
